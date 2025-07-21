5bcc856a11a4077d8304ac83b36e5f1461bd9946
Danil Bogodvid <d.bogodvid@kefirgames.com>
2025-07-21 12:15:04 +0300

    fix: allocation new classes

--- Assets/Scripts/FishingStatesSystem/ResultState.cs
+++ Assets/Scripts/FishingStatesSystem/ResultState.cs
@@ -6,24 +6,22 @@ namespace FishingStatesSystem
 {
     public class ResultState : StateBase
     {
-        private readonly bool _success;
 
-        public ResultState(StatesController controller, FishingActions actions, bool success, FishingSettings fishingSettings)
+        public ResultState(StatesController controller, FishingActions actions, FishingSettings fishingSettings)
             : base(controller, actions, fishingSettings)
         {
-            _success = success;
         }
 
         public override void Enter()
         {
             controller.StopMinigame();
-            if (_success)
+            if (controller.GetResultSuccessAndReset(false))
                 FishingEventsBank.RaiseOnCatchSuccess();
             else
                 FishingEventsBank.RaiseOnCatchFail();
 
             actions.ReturnFloat();
-            controller.SetState(new IdleState(controller, actions, fishingSettings));
+            controller.SetState(FishingStates.Idle);
         }
     }
 }
--- Assets/Scripts/FishingStatesSystem/StatesController.cs
+++ Assets/Scripts/FishingStatesSystem/StatesController.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using BiteSystem;
+using InitializeSystem;
 using Minigame;
 using ProjectSerializebleDataObjects;
 using UnityEngine;
@@ -11,13 +13,35 @@ namespace FishingStatesSystem
         private FishingMinigameController _fishingMinigameController;
         private BiteController _biteController;
         private FishingSettings _fishingSettings;
+        private FishingActions _fishingActions;
+        
+        private bool _isResultSuccess;
+        private Dictionary<FishingStates,StateBase> _states;
 
-        public StatesController(FishingMinigameController minigameController, BiteController biteController, FishingSettings fishingSettings)
+        public StatesController(FishingMinigameController minigameController, BiteController biteController, FishingSettings fishingSettings,
+            FishingActions fishingActions)
         {
             _fishingMinigameController = minigameController;
             _biteController = biteController;
             _fishingSettings = fishingSettings;
+            _fishingActions = fishingActions;
             _fishingMinigameController.StopMinigame();
+            _states = new Dictionary<FishingStates, StateBase>();
+            _states.Add(FishingStates.Idle, new IdleState(this, _fishingActions, _fishingSettings));
+            _states.Add(FishingStates.Casting, new CastingState(this, _fishingActions, _fishingSettings));
+            _states.Add(FishingStates.Waiting, new WaitingState(this, _fishingActions, _fishingSettings));
+            _states.Add(FishingStates.Bite, new BiteState(this, _fishingActions, _fishingSettings));
+            _states.Add(FishingStates.Holding, new HoldingState(this, _fishingActions, _fishingSettings));
+            _states.Add(FishingStates.Minigame, new MinigameState(this, _fishingActions, _fishingSettings));
+            _states.Add(FishingStates.Result, new ResultState(this, _fishingActions, _fishingSettings));
+            _currentState = _states[FishingStates.Idle];
+        }
+
+        public bool GetResultSuccessAndReset(bool success)
+        {
+            bool previousResult = _isResultSuccess;
+            _isResultSuccess = success;
+            return previousResult;
         }
 
         public void StartMinigame()
@@ -34,13 +58,13 @@ namespace FishingStatesSystem
         {
             _biteController.BiteRiseAwaiter(_fishingSettings.biteDelayMin, _fishingSettings.biteDelayMax);
         }
-
-        public void SetState(StateBase newState)
+        
+        public void SetState(FishingStates newState)
         {
-            Debug.Log(newState.GetType().Name);
-            newState.Exit();
-            _currentState = newState;
-            newState.Enter();
+            Debug.Log(newState);
+            _currentState.Exit();
+            _currentState = _states[newState];
+            _currentState.Enter();
         }
 
         public void UpdateState()
